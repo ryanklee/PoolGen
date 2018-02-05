0ad2d301373641234169f8cf414ccd146343e706
Ryan Kleeberger <rylklee@gmail.com>
2018-02-05 13:52:37 -0600

    domain sketch done, tear down for actual rgr cycling

--- PoolGen.Tests/PoolGenTests.cs
+++ PoolGen.Tests/PoolGenTests.cs
@@ -9,24 +9,10 @@ namespace PoolGen.Tests
     public class PoolGenTests
     {
         [Fact]
-        public void PoolGen_Returns_Containing_Single_Game_For_Two_Teams_One_Round()
+        public void PoolGen_Returns_List_of_Type_Pool()
         {
             PoolGenerator generator = new PoolGenerator();
 
-            var numOfPools = 1;
-            var numOfTeams = 2;
-            var numOfRounds = 1;
-
-            var team1 = new Team() { Name = "Team 1" };
-            var team2 = new Team() { Name = "Team 2" };
-            var teams = new List<Team>() { team1, team2 };
-            var game1 = new Game() { Id = 1, Teams = teams };
-            var pool1 = new Pool() { Name = "Pool A", Games = { game1 }, Teams = teams };
-
-            var expected = new List<Pool>() { pool1 }; 
-            var actual = generator.GeneratePools(numOfPools, numOfTeams, numOfRounds);
-
-            Assert.Equal(expected, actual);
         }
     }
 }
--- PoolGen/Services/PoolGenerator.cs
+++ PoolGen/Services/PoolGenerator.cs
@@ -9,55 +9,10 @@ namespace PoolGen.Services
 {
     public class PoolGenerator : IPoolGenerator
     {
-        public List<Pool> GeneratePools(int numOfPools, int numOfteams, int numOfRounds)
-        {
-
-            var pools = CreatePoolObjects(numOfPools);
-            pools = AssignPoolNames(pools);
-            
-            var teams = CreateTeamObjects(numOfteams);
-            teams = AssignTeamNames(teams);
-
-
-
-            return pools;
-        }
-
-        private List<Team> AssignTeamNames(List<Team> teams)
-        {
-            teams.ForEach(team => team.Name = "Team " + teams.IndexOf(team));
-            return teams;
-        }
-
-        private List<Team> CreateTeamObjects(int numOfteams)
-        {
-            var teams = new List<Team>();
-            for (int i = 0; i < numOfteams; i++)
-            {
-                teams.Add(new Team());
-            }
-            return teams;
-        }
-
-        private List<Pool> CreatePoolObjects(int numOfPools)
+        public List<Pool> GeneratePools()
         {
             var pools = new List<Pool>();
-            for (int i = 0; i < numOfPools; i++)
-            {
-                pools.Add(new Pool());
-            }
             return pools;
-        }
-
-        private List<Pool> AssignPoolNames(List<Pool> pools)
-        {
-            pools.ForEach(pool => pool.Name = "Pool " + GetLetter(pools.IndexOf(pool)));
-            return pools;
-        }
-
-        private char GetLetter(int numToConvert)
-        {
-            return Convert.ToChar(numToConvert + 41);
         }
     }
 }
